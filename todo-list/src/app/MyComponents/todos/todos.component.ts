0c6cfac920868be54e01bda53c2ec3e45fe4491f
daadestroyer <nigamshubham2000@gmail.com>
2021-05-01 01:48:11 +0530

    deleting functionality completed...

--- todo-list/src/app/MyComponents/todos/todos.component.ts
+++ todo-list/src/app/MyComponents/todos/todos.component.ts
@@ -35,5 +35,8 @@ export class TodosComponent implements OnInit {
 
   deleteTodo(todo: Todo) {
     console.log(todo);
+    const index = this.todos.indexOf(todo)
+    this.todos.splice(index,1)
   }
+
 }
