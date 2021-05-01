4fa946477742f0fafa0a4553fa3465aa6129941a
daadestroyer <nigamshubham2000@gmail.com>
2021-05-01 12:07:47 +0530

    added dashboard component also...

--- todo-list/src/app/app-routing.module.ts
+++ todo-list/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './MyComponents/about/about.component';
+import { DashboardComponent } from './MyComponents/dashboard/dashboard.component';
 import { TodosComponent } from './MyComponents/todos/todos.component';
 
 
 const routes: Routes = [
-  { path: '', component: TodosComponent },
+  { path: '', component: DashboardComponent },
+  { path: 'addtodo', component: TodosComponent },
   { path: 'about', component: AboutComponent },
 ];
 @NgModule({
